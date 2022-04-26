60549ce925b217cdff5dd75cca3bab7e4ca1a102
Fin-NewWin <nguyenphien17@yahoo.com>
2022-04-25 21:24:36 -0700

    renamed with sh file modifier

---
+++ script/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Valid Phone number
+# Leetcode
+# (000) 000-0000
+# 000-000-0000
+# grep with regex
+# This was for leetcode solutions
+# grep -P "^\(\b\d{3}\b\) \b\d{3}-\d{4}$|^\d{3}-\d{3}-\d{4}$" file.txt
+
+echo `date`
