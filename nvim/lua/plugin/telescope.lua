0c6e1f36d5f24aa3c4bb1fd1306fda312ad2516c
Fin-NewWin <nguyenphien17@yahoo.com>
2022-08-08 21:20:32 -0700

    Rice: changed telescope preview to be top rather than on the bottom

--- nvim/lua/plugin/telescope.lua
+++ nvim/lua/plugin/telescope.lua
@@ -12,8 +12,25 @@ telescope.setup{
         selection_caret = " ",
         file_sorter = require("telescope.sorters").get_fzy_sorter,
         file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        path_display = { "truncate" },
         grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
         qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
         mappings = {
             i = {
                 ['<C-[>'] = tele_actions.close,
@@ -31,6 +48,8 @@ telescope.setup{
             }
         },
         color_devicons = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        winblend = 0,
         vimgrep_arguments = {
             'rg',
             '--color=never',
@@ -41,6 +60,7 @@ telescope.setup{
             '--smart-case',
             '--hidden',
         },
+        file_ignore_patterns = { "node_modules" },
     }
 }
 telescope.load_extension('fzy_native')
