f9b13b76cfc2740050e0e44a1caa5ee9579dd26f
Fin-NewWin <nguyenphien17@yahoo.com>
2022-08-30 14:50:17 -0700

    chore: refactored the way to change the colors using plugins lua functions

--- nvim/lua/plugin/colorscheme.lua
+++ nvim/lua/plugin/colorscheme.lua
@@ -1,14 +1,25 @@
+local status_ok, gruvbox = pcall(require, "gruvbox")
+if not status_ok then
+    return
+end
+
+gruvbox.setup({
+    overrides = {
+
+        NormalFloat     = {bg = "NONE"},
+        SignColumn      = {bg = "NONE"},
+        FloatBorder     = {TelescopeBorder},
+
+        -- Git Colors
+        GitSignsAdd     = {fg = "#b8bb26", bg = "NONE"},
+        GitSignsChange  = {fg = "#fe8019", bg = "NONE"},
+        GitSignsDelete  = {fg = "#fb4934", bg = "NONE"},
+
+    }
+})
+
 local function colorscheme(name)
     vim.g.colors_name = name
     vim.o.background = vim.o.background
 end
 colorscheme('gruvbox')
-
-vim.api.nvim_set_hl(0, "NormalFloat", { bg="NONE" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg="NONE" })
-vim.api.nvim_set_hl(0, "FloatBorder", { TelescopeBorder })
-
--- GitSigns bg removal
-vim.api.nvim_set_hl(0, "GitSignsAdd", { fg="#b8bb26", bg="NONE" })
-vim.api.nvim_set_hl(0, "GitSignsChange", { fg="#fe8019", bg="NONE" })
-vim.api.nvim_set_hl(0, "GitSignsDelete", { fg="#fb4934", bg="NONE" })
