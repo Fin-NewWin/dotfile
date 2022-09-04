f626548f7e913408fb6bb713a8a5a4aef6aea943
Fin-NewWin <nguyenphien17@yahoo.com>
2022-09-03 22:01:06 -0700

    chore: renamed variable to stop sumneko complaining

--- nvim/lua/plugin/lspconfig.lua
+++ nvim/lua/plugin/lspconfig.lua
@@ -47,9 +47,9 @@ local win = require('lspconfig.ui.windows')
 local _default_opts = win.default_opts
 
 win.default_opts = function(options)
-    local opts = _default_opts(options)
-    opts.border = 'single'
-    return opts
+    local default_opts = _default_opts(options)
+    default_opts.border = 'single'
+    return default_opts
 end
 
 -- Pipe commands into telescope
@@ -90,6 +90,27 @@ for _, lsp in pairs(servers) do
         }
     }
 end
+
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = runtime_path,
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = { library = vim.api.nvim_get_runtime_file('', true) },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = { enable = false, },
+    },
+  },
+}
 
 -- nvim-cmp setup
 local cmp = require('cmp')
