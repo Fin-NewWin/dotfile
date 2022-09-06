4a9182f33b495d73b9eee65b8af75853c4e856bd
Fin-NewWin <nguyenphien17@yahoo.com>
2022-09-06 13:00:44 -0700

    feat(gitsigns): changed symbol to all the same got git and just add different colors with priority

--- nvim/lua/plugin/gitsigns.lua
+++ nvim/lua/plugin/gitsigns.lua
@@ -5,10 +5,30 @@ end
 
 gitsigns.setup {
     signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '-' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
+        add = {
+            hl = "GitSignsAdd"   , text = "▍", numhl="GitSignsAddNr"   , linehl="GitSignsAddLn"
+        },
+        change = {
+            hl = "GitSignsChange", text = "▍", numhl="GitSignsChangeNr", linehl="GitSignsChangeLn"
+        },
+        delete = {
+            hl = "GitSignsDelete", text = "▍", numhl="GitSignsDeleteNr", linehl="GitSignsDeleteLn"
+        },
+        changedelete = {
+            hl = "GitSignsChange", text = "▍", numhl="GitSignsChangeNr", linehl="GitSignsChangeLn"
+        },
+        topdelete = {
+            hl = "GitSignsDelete", text = "‾", numhl="GitSignsDeleteNr", linehl="GitSignsDeleteLn"
+        },
     },
+    watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+    },
+    diff_opts = {
+        algorithm = "histogram",
+        internal = true,
+        indent_heuristic = true,
+    },
+    sign_priority = 6,
 }
