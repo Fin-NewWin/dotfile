6289d511abc99d91cbd922ef3cf31b4da8ead88a
Fin-NewWin <nguyenphien17@yahoo.com>
2022-09-08 00:36:59 -0700

    feat(prompt): started using starship prompt made on rust

--- zsh/zsh/plugins.zsh
+++ zsh/zsh/plugins.zsh
@@ -2,9 +2,3 @@
 zsh_add_plugin "zsh-users/zsh-autosuggestions"
 ZSH_AUTOSUGGEST_MANUAL_REBIND=none
 ZSH_AUTOSUGGEST_BUFFER_MAX_SIZE=20
-
-## zsh_add_plugin "marlonrichert/zsh-autocomplete"
-zsh_add_plugin "zsh-users/zsh-syntax-highlighting"
-(( ${+ZSH_HIGHLIGHT_STYLES} )) || typeset -A ZSH_HIGHLIGHT_STYLES
-ZSH_HIGHLIGHT_STYLES[path]=none
-ZSH_HIGHLIGHT_STYLES[path_prefix]=none
