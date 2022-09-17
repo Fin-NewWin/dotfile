a17b342645a9d35340a40846f89ffa6c57821bde
Fin-NewWin <nguyenphien17@yahoo.com>
2022-09-17 03:25:30 -0700

    fix: remove lsp signs bg

--- nvim/lua/plugin/config/colorscheme.lua
+++ nvim/lua/plugin/config/colorscheme.lua
@@ -15,6 +15,12 @@ gruvbox.setup({
         GitSignsChange  = {fg = "#fe8019", bg = "NONE"},
         GitSignsDelete  = {fg = "#fb4934", bg = "NONE"},
 
+        -- LSP
+        DiagnosticSignError =   { fg="#fb4934" },
+        DiagnosticSignWarn  =   { fg="#fabd2f" },
+        DiagnosticSignHint  =   { fg="#8ec07c" },
+        DiagnosticSignInfo  =   { fg="#83a598" },
+
     }
 })
 
@@ -22,4 +28,5 @@ local function colorscheme(name)
     vim.g.colors_name = name
     vim.o.background = vim.o.background
 end
+
 colorscheme('gruvbox')
