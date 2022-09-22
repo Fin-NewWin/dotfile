f62073e5824689ae15c7b8db52a22b460bcba9ff
Fin-NewWin <nguyenphien17@yahoo.com>
2022-09-22 16:40:48 -0700

    chore: fixed spacing

--- nvim/lua/plugin/config/gitsigns.lua
+++ nvim/lua/plugin/config/gitsigns.lua
@@ -4,21 +4,11 @@ if not status_ok then
 end
 return gitsigns.setup {
     signs = {
-        add = {
-            hl = "GitSignsAdd"   , text = "▍", numhl="GitSignsAddNr"   , linehl="GitSignsAddLn"
-        },
-        change = {
-            hl = "GitSignsChange", text = "▍", numhl="GitSignsChangeNr", linehl="GitSignsChangeLn"
-        },
-        delete = {
-            hl = "GitSignsDelete", text = "▍", numhl="GitSignsDeleteNr", linehl="GitSignsDeleteLn"
-        },
-        changedelete = {
-            hl = "GitSignsChange", text = "▍", numhl="GitSignsChangeNr", linehl="GitSignsChangeLn"
-        },
-        topdelete = {
-            hl = "GitSignsDelete", text = "‾", numhl="GitSignsDeleteNr", linehl="GitSignsDeleteLn"
-        },
+        add             = { hl = "GitSignsAdd"   , text = "▍", numhl="GitSignsAddNr"   , linehl="GitSignsAddLn" },
+        change          = { hl = "GitSignsChange", text = "▍", numhl="GitSignsChangeNr", linehl="GitSignsChangeLn" },
+        delete          = { hl = "GitSignsDelete", text = "▍", numhl="GitSignsDeleteNr", linehl="GitSignsDeleteLn" },
+        changedelete    = { hl = "GitSignsChange", text = "▍", numhl="GitSignsChangeNr", linehl="GitSignsChangeLn" },
+        topdelete       = { hl = "GitSignsDelete", text = "‾", numhl="GitSignsDeleteNr", linehl="GitSignsDeleteLn" },
     },
     watch_gitdir = {
         interval = 1000,
