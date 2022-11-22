4d612772f02a7165359de2706996e2cd82363578
Fin-NewWin <nguyenphien17@yahoo.com>
2022-11-22 00:28:43 -0800

    feat: added colored icons to alpha

--- nvim/lua/user/plugin/config/alpha.lua
+++ nvim/lua/user/plugin/config/alpha.lua
@@ -8,9 +8,15 @@ if not path_ok then
     return
 end
 
+
 local dashboard = require("alpha.themes.dashboard")
-local nvim_web_devicons = require("nvim-web-devicons")
-local cdir = vim.fn.getcwd()
+-- local nvim_web_devicons = require("nvim-web-devicons")
+local nvim_web_devicons = {
+	enabled = true,
+	highlight = true,
+}
+
+local if_nil = vim.F.if_nil
 
 local function getGreeting(name)
     local tableTime = os.date("*t")
@@ -49,120 +55,6 @@ local greetHeading = {
     },
 }
 
-local function get_extension(fn)
-    local match = fn:match("^.+(%..+)$")
-    local ext = ""
-    if match ~= nil then
-        ext = match:sub(2)
-    end
-    return ext
-end
-
-local function icon(fn)
-    local nwd = require("nvim-web-devicons")
-    local ext = get_extension(fn)
-    return nwd.get_icon(fn, ext, { default = true })
-end
-
-local function file_button(fn, sc, short_fn)
-    short_fn = short_fn or fn
-    local ico_txt
-    local fb_hl = {}
-
-    local ico, hl = icon(fn)
-    local hl_option_type = type(nvim_web_devicons.highlight)
-    if hl_option_type == "boolean" then
-        if hl and nvim_web_devicons.highlight then
-            table.insert(fb_hl, { hl, 0, 1 })
-        end
-    end
-    if hl_option_type == "string" then
-        table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 1 })
-    end
-    ico_txt = ico .. "  "
-
-    local file_button_el = dashboard.button(sc, ico_txt .. short_fn, "<cmd>e " .. fn .. " <CR>")
-    local fn_start = short_fn:match(".*/")
-    if fn_start ~= nil then
-        table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt - 2 })
-    end
-    file_button_el.opts.hl = fb_hl
-    return file_button_el
-end
-
-local default_mru_ignore = { "gitcommit" }
-
-local mru_opts = {
-    ignore = function(path, ext)
-        return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
-    end,
-}
-
---- @param start number
---- @param cwd string optional
---- @param items_number number optional number of items to generate, default = 10
-local function mru(start, cwd, items_number, opts)
-    opts = opts or mru_opts
-    items_number = items_number or 9
-
-    local oldfiles = {}
-    for _, v in pairs(vim.v.oldfiles) do
-        if #oldfiles == items_number then
-            break
-        end
-        local cwd_cond
-        if not cwd then
-            cwd_cond = true
-        else
-            cwd_cond = vim.startswith(v, cwd)
-        end
-        local ignore = (opts.ignore and opts.ignore(v, get_extension(v))) or false
-        if (vim.fn.filereadable(v) == 1) and cwd_cond and not ignore then
-            oldfiles[#oldfiles + 1] = v
-        end
-    end
-
-    local special_shortcuts = { "a", "s", "d" }
-    local target_width = 35
-
-    local tbl = {}
-    for i, fn in ipairs(oldfiles) do
-        local short_fn
-        if cwd then
-            short_fn = vim.fn.fnamemodify(fn, ":.")
-        else
-            short_fn = vim.fn.fnamemodify(fn, ":~")
-        end
-
-        if #short_fn > target_width then
-            short_fn = path.new(short_fn):shorten(1, { -2, -1 })
-            if #short_fn > target_width then
-                short_fn = path.new(short_fn):shorten(1, { -1 })
-            end
-        end
-
-        local shortcut = ""
-        if i <= #special_shortcuts then
-            shortcut = special_shortcuts[i]
-        else
-            shortcut = tostring(i + start - 1 - #special_shortcuts)
-        end
-
-        local file_button_el = file_button(fn, " " .. shortcut, short_fn)
-        tbl[i] = file_button_el
-    end
-    return {
-        type = "group",
-        val = tbl,
-        opts = {},
-    }
-end
-
-math.randomseed(os.time())
-local function pick_color()
-    local colors = {"String", "Identifier", "Keyword", "Number"}
-    return colors[math.random(#colors)]
-end
 local header = {
     type = "text",
     val = {
@@ -193,6 +85,120 @@ local header = {
     },
 }
 
+local function get_extension(fn)
+	local match = fn:match("^.+(%..+)$")
+	local ext = ""
+	if match ~= nil then
+		ext = match:sub(2)
+	end
+	return ext
+end
+
+local function icon(fn)
+	local nwd = require("nvim-web-devicons")
+	local ext = get_extension(fn)
+	return nwd.get_icon(fn, ext, { default = true })
+end
+
+local function file_button(fn, sc, short_fn)
+	short_fn = short_fn or fn
+	local ico_txt
+	local fb_hl = {}
+
+	if nvim_web_devicons.enabled then
+		local ico, hl = icon(fn)
+		local hl_option_type = type(nvim_web_devicons.highlight)
+		if hl_option_type == "boolean" then
+			if hl and nvim_web_devicons.highlight then
+				table.insert(fb_hl, { hl, 0, 3 })
+			end
+		end
+		if hl_option_type == "string" then
+			table.insert(fb_hl, { nvim_web_devicons.highlight, 0, 3 })
+		end
+		ico_txt = ico .. "  "
+	else
+		ico_txt = ""
+	end
+	local file_button_el = dashboard.button(sc, ico_txt .. short_fn, "<cmd>e " .. fn .. " <CR>")
+	local fn_start = short_fn:match(".*[/\\]")
+	if fn_start ~= nil then
+		table.insert(fb_hl, { "Comment", #ico_txt - 2, #fn_start + #ico_txt })
+	end
+	file_button_el.opts.hl = fb_hl
+	return file_button_el
+end
+
+local default_mru_ignore = { "gitcommit" }
+
+
+local mru_opts = {
+	ignore = function(path, ext)
+		return (string.find(path, "COMMIT_EDITMSG")) or (vim.tbl_contains(default_mru_ignore, ext))
+	end,
+}
+
+--- @param start number
+--- @param cwd string optional
+--- @param items_number number optional number of items to generate, default = 10
+local function mru(start, cwd, items_number, opts)
+	opts = opts or mru_opts
+	items_number = if_nil(items_number, 11)
+
+	local oldfiles = {}
+	for _, v in pairs(vim.v.oldfiles) do
+		if #oldfiles == items_number then
+			break
+		end
+		local cwd_cond
+		if not cwd then
+			cwd_cond = true
+		else
+			cwd_cond = vim.startswith(v, cwd)
+		end
+		local ignore = (opts.ignore and opts.ignore(v, get_extension(v))) or false
+		if (vim.fn.filereadable(v) == 1) and cwd_cond and not ignore then
+			oldfiles[#oldfiles + 1] = v
+		end
+	end
+    local special_shortcuts = { "a", "s", "d" }
+	local target_width = 39
+
+	local tbl = {}
+	for i, fn in ipairs(oldfiles) do
+        local short_fn
+        if cwd then
+            short_fn = vim.fn.fnamemodify(fn, ":.")
+        else
+            short_fn = vim.fn.fnamemodify(fn, ":~")
+        end
+
+        if #short_fn > target_width then
+            short_fn = path.new(short_fn):shorten(1, { -2, -1 })
+            if #short_fn > target_width then
+                short_fn = path.new(short_fn):shorten(1, { -1 })
+            end
+        end
+
+        local shortcut = ""
+        if i <= #special_shortcuts then
+            shortcut = special_shortcuts[i]
+        else
+            shortcut = tostring(i + start - 1 - #special_shortcuts)
+        end
+
+        local file_button_el = file_button(fn, " " .. shortcut, short_fn)
+        tbl[i] = file_button_el
+	end
+	return {
+		type = "group",
+		val = tbl,
+		opts = {},
+	}
+end
+
+local cwd = vim.fn.getcwd()
+
 local section_mru = {
     type = "group",
     val = {
@@ -209,7 +215,7 @@ local section_mru = {
         {
             type = "group",
             val = function()
-                return { mru(1, cdir, 6) }
+                return { mru(1, cwd, 6) }
             end,
             opts = { shrink_margin = false },
         },
@@ -258,27 +264,20 @@ local fortune = {
     },
 }
 
-local pad = {
-    type = "padding",
-    val = 1,
-}
 
 local opts = {
     layout = {
-        pad,
+        { type = "padding", val = 1 },
         header,
-        pad,
+        { type = "padding", val = 2 },
         greetHeading,
         footer,
-        pad,
+        { type = "padding", val = 2 },
         section_mru,
-        pad,
+        { type = "padding", val = 2 },
         buttons,
-        pad,
+        { type = "padding", val = 1 },
         fortune,
-    },
-    opts = {
-        margin = 5,
     },
 }
 
@@ -300,4 +299,3 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt.laststatus = 0
     end,
 })
-
