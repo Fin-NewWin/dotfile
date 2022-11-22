b31118161aa1e6c253ca3618b2636485c8dd9da3
Fin-NewWin <nguyenphien17@yahoo.com>
2022-11-22 02:26:26 -0800

    feat: packer installs to path rather than bootstrap

--- nvim/lua/user/plugin/init.lua
+++ nvim/lua/user/plugin/init.lua
@@ -1,23 +1,30 @@
--- Bootstrap
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+local fn = vim.fn
+-- Automatically install packer
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+    PACKER_BOOTSTRAP = fn.system({
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    })
+    print("Installing packer close and reopen Neovim...")
+    vim.cmd([[packadd packer.nvim]])
 end
-
-local packer_bootstrap = ensure_packer()
 
 -- Function to get config
 local function get_config(name)
     return string.format('require("user.plugin.config.%s")', name)
 end
 
-local packer = require('packer')
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
 
 packer.startup{
     function(use)
@@ -110,7 +117,12 @@ packer.startup{
     end,
     config = {
         display = {
-            prompt_border = 'rounded'
+            prompt_border = 'rounded',
+            open_fn = function()
+                return require("packer.util").float({ border = "rounded" })
+            end,
         }
-    }
+    },
+    auto_clean = true,
+    compile_on_sync = true,
 }
