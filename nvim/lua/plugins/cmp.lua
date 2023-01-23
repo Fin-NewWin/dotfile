82782e55f5b98b3323c422082adedbf3cce87658
Fin-NewWin <nguyenphien17@yahoo.com>
2023-01-23 01:53:46 -0800

    feat: cmp add cmpline and loads when enter cmd mode

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,8 +1,10 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    event = {
+        "InsertEnter",
+        "CmdlineEnter",
+    },
     dependencies = {
-
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-nvim-lua",
         "hrsh7th/cmp-path",
