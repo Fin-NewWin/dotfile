1a4995d0d34787aae30e482e539fe29b391658d0
Phien Nguyen <nguyenphien17@gmail.com>
2023-03-15 00:35:17 -0700

    format with stylua

--- nvim/lua/plugin/line.lua
+++ nvim/lua/plugin/line.lua
@@ -5,38 +5,38 @@ return {
         config = true,
     },
     {
-            "utilyre/barbecue.nvim",
-            event = "VeryLazy",
-            name = "barbecue",
-            version = "*",
-            dependencies = {
-                "SmiteshP/nvim-navic",
-                "nvim-tree/nvim-web-devicons",
-            },
-            config = function()
-                -- triggers CursorHold event faster
-                vim.opt.updatetime = 200
+        "utilyre/barbecue.nvim",
+        event = "VeryLazy",
+        name = "barbecue",
+        version = "*",
+        dependencies = {
+            "SmiteshP/nvim-navic",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            -- triggers CursorHold event faster
+            vim.opt.updatetime = 200
 
-                require("barbecue").setup({
-                    create_autocmd = false,
-                    attach_navic = false,
-                })
+            require("barbecue").setup {
+                create_autocmd = false,
+                attach_navic = false,
+            }
 
-                vim.api.nvim_create_autocmd({
-                    "WinResized",
-                    "BufWinEnter",
-                    "CursorHold",
-                    "InsertLeave",
+            vim.api.nvim_create_autocmd({
+                "WinResized",
+                "BufWinEnter",
+                "CursorHold",
+                "InsertLeave",
 
-                    "BufWritePost",
-                    "TextChanged",
-                    "TextChangedI",
-                }, {
-                    group = vim.api.nvim_create_augroup("barbecue.updater", {}),
-                    callback = function()
-                        require("barbecue.ui").update()
-                    end,
-                })
-            end
+                "BufWritePost",
+                "TextChanged",
+                "TextChangedI",
+            }, {
+                group = vim.api.nvim_create_augroup("barbecue.updater", {}),
+                callback = function()
+                    require("barbecue.ui").update()
+                end,
+            })
+        end,
     },
 }
