2bbc6d86d78e977841a5631b6aeb34c33652734d
Phien Nguyen <nguyenphien17@yahoo.com>
2023-08-26 17:01:35 -0700

    chore: stylua

--- nvim/lua/plugin/lsp.lua
+++ nvim/lua/plugin/lsp.lua
@@ -85,14 +85,14 @@ return {
 
             local cmp = require("cmp")
 
-            require('luasnip.loaders.from_vscode').lazy_load()
+            require("luasnip.loaders.from_vscode").lazy_load()
 
             cmp.setup({
                 sources = {
-                    { name = "nvim_lsp", },
-                    { name = "luasnip", },
-                    { name = "path", },
-                    { name = "buffer", },
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "path" },
+                    { name = "buffer" },
                 },
             })
 
@@ -100,18 +100,17 @@ return {
             local cmp_mappings = lsp.defaults.cmp_mappings({
                 ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
                 ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({select = true}),
+                ["<C-y>"] = cmp.mapping.confirm({ select = true }),
             })
 
-            cmp_mappings['<Tab>'] = nil
-            cmp_mappings['<S-Tab>'] = nil
+            cmp_mappings["<Tab>"] = nil
+            cmp_mappings["<S-Tab>"] = nil
 
             lsp.setup_nvim_cmp({
                 mapping = cmp_mappings,
                 documentation = false,
                 use_luasnip = true,
             })
-
 
             lsp.on_attach(function(client, bufnr)
                 local opts = { buffer = bufnr, remap = false }
