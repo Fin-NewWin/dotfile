23fd7333052aeb35105c134e59d099c4fe0db94f
Phien Nguyen <nguyenphien17@yahoo.com>
2023-10-07 19:56:26 -0700

    remove: plugins

--- nvim/lua/plugin/ui.lua
+++ nvim/lua/plugin/ui.lua
@@ -39,19 +39,6 @@ return {
         end,
     },
     {
-        "akinsho/toggleterm.nvim",
-        event = "VeryLazy",
-        opts = {
-            size = 20,
-            on_create = function()
-                vim.opt.foldcolumn = "0"
-                vim.opt.signcolumn = "no"
-            end,
-            open_mapping = [[<F7>]],
-            direction = "horizontal",
-        },
-    },
-    {
         "NvChad/nvim-colorizer.lua",
         event = { "BufReadPre", "BufNewFile" },
         config = true,
