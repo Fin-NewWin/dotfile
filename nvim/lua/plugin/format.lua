7b4c3827a668814ef7b287a86971ab70fb56546b
Phien Nguyen <nguyenphien17@yahoo.com>
2023-10-09 11:25:06 -0700

    feat: auto install format

--- nvim/lua/plugin/format.lua
+++ nvim/lua/plugin/format.lua
@@ -1,14 +1,24 @@
+local formatters_by_ft = {
+    javascript = { "prettier" },
+    typescript = { "prettier" },
+    javascriptreact = { "prettier" },
+    typescriptreact = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    json = { "prettier" },
+    lua = { "stylua" },
+    python = { "black" },
+}
 return {
     {
         "stevearc/conform.nvim",
         lazy = false,
         event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
         opts = {
-            formatters_by_ft = {
-                javascript = { "prettier" },
-                typescript = { "prettier" },
-                lua = { "stylua" },
-            },
+            formatters_by_ft = formatters_by_ft,
             format_on_save = {
                 lsp_fallback = false,
                 async = true,
@@ -16,9 +26,16 @@ return {
             },
         },
         config = function(_, opts)
+            local mason_tool_installer = require("mason-tool-installer")
+
             local util = require("conform.util")
             util.add_formatter_args(require("conform.formatters.prettier"), { "--tab-width=4" })
             require("conform").setup(opts)
+
+            -- TODO: wait for mason plugin for conform, for not workaround
+            mason_tool_installer.setup({
+                ensure_installed = vim.tbl_values(formatters_by_ft),
+            })
         end,
     },
 }
