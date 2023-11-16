28e803f01734851d54bb6a6a5d1319a2540f8315
Phien Nguyen <nguyenphien17@yahoo.com>
2023-11-16 11:00:27 -0800

    chore: update

--- nvim/lua/core/autocmd.lua
+++ nvim/lua/core/autocmd.lua
@@ -4,112 +4,101 @@ local augroup = vim.api.nvim_create_augroup
 -- Filetype
 -- nesC not supported, temp make it into filetype "c"
 autocmd({ "BufEnter" }, {
-    pattern = {
-        "*.nc",
-    },
-    callback = function()
-        vim.bo.filetype = "c"
-    end,
+  pattern = {
+    "*.nc",
+  },
+  callback = function()
+    vim.bo.filetype = "c"
+  end,
 })
 
 autocmd("BufWritePre", {
-    desc = "Remove trailing spaces",
-    -- pattern = "*",
-    command = "%s/\\s\\+$//e",
+  desc = "Remove trailing spaces",
+  -- pattern = "*",
+  command = "%s/\\s\\+$//e",
 })
 
 autocmd("BufEnter", {
-    desc = "Remove auto comments",
-    callback = function()
-        vim.opt.formatoptions:remove({ "c", "r", "o" })
-    end,
+  desc = "Remove auto comments",
+  callback = function()
+    vim.opt.formatoptions:remove({ "c", "r", "o" })
+  end,
 })
 
 autocmd("TermOpen", {
-    desc = "Terminal settings",
-    callback = function()
-        vim.opt_local.relativenumber = false
-        vim.opt_local.number = false
-        vim.cmd("startinsert!")
-    end,
+  desc = "Terminal settings",
+  callback = function()
+    vim.opt_local.relativenumber = false
+    vim.opt_local.number = false
+    vim.cmd("startinsert!")
+  end,
 })
 
 local highlight_group = augroup("YankHighlight", { clear = true })
 autocmd("TextYankPost", {
-    desc = "Highlight yank",
-    callback = function()
-        vim.highlight.on_yank()
-    end,
-    group = highlight_group,
-    pattern = "*",
+  desc = "Highlight yank",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = "*",
 })
 
 autocmd("FocusGained", {
-    desc = "Update file when there are changes",
-    callback = function()
-        vim.cmd("checktime")
-    end,
-})
-
--- TODO: Fix later if doesn't work for yaml
-autocmd("FileType", {
-    pattern = "yaml",
-    callback = function()
-        vim.opt_local.expandtab = true
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.tabstop = 4
-        vim.opt_local.softtabstop = 4
-    end,
+  desc = "Update file when there are changes",
+  callback = function()
+    vim.cmd("checktime")
+  end,
 })
 
 autocmd({ "BufRead", "BufNewFile" }, {
-    desc = "Enable spell checking in filetypes",
-    pattern = { "*.txt", "*.md", "*.tex" },
-    callback = function()
-        vim.opt_local.spell = true
-    end,
+  desc = "Enable spell checking in filetypes",
+  pattern = { "*.txt", "*.md", "*.tex" },
+  callback = function()
+    vim.opt_local.spell = true
+  end,
 })
 
 autocmd({ "InsertLeave", "WinEnter" }, {
-    desc = "Show cursor line only in active window",
-    callback = function()
-        local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
-        if ok and cl then
-            vim.wo.cursorline = true
-            vim.api.nvim_win_del_var(0, "auto-cursorline")
-        end
-    end,
+  desc = "Show cursor line only in active window",
+  callback = function()
+    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, "auto-cursorline")
+    if ok and cl then
+      vim.wo.cursorline = true
+      vim.api.nvim_win_del_var(0, "auto-cursorline")
+    end
+  end,
 })
 
 autocmd({ "InsertEnter", "WinLeave" }, {
-    desc = "Disable cursorline when buffer not focused",
-    callback = function()
-        local cl = vim.wo.cursorline
-        if cl then
-            vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
-            vim.wo.cursorline = false
-        end
-    end,
+  desc = "Disable cursorline when buffer not focused",
+  callback = function()
+    local cl = vim.wo.cursorline
+    if cl then
+      vim.api.nvim_win_set_var(0, "auto-cursorline", cl)
+      vim.wo.cursorline = false
+    end
+  end,
 })
 
 autocmd({ "FileType" }, {
-    desc = "Fix conceallevel for json and help files",
-    pattern = { "json", "jsonc" },
-    callback = function()
-        vim.wo.spell = false
-        vim.wo.conceallevel = 0
-    end,
+  desc = "Fix conceallevel for json and help files",
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
 })
 
 autocmd("BufReadPost", {
-    desc = "Return to last edit location",
-    callback = function()
-        local mark = vim.api.nvim_buf_get_mark(0, '"')
-        local lcount = vim.api.nvim_buf_line_count(0)
-        if mark[1] > 0 and mark[1] <= lcount then
-            pcall(vim.api.nvim_win_set_cursor, 0, mark)
-        end
-    end,
+  desc = "Return to last edit location",
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
 })
 
 -- not really an autocmd
@@ -117,12 +106,12 @@ autocmd("BufReadPost", {
 local ns = vim.api.nvim_create_namespace("toggle_hlsearch")
 
 local function toggle_hlsearch(char)
-    if vim.fn.mode() == "n" then
-        local keys = { "<CR>", "n", "N", "*", "#", "?", "/" }
-        local if_search = vim.tbl_contains(keys, vim.fn.keytrans(char))
+  if vim.fn.mode() == "n" then
+    local keys = { "<CR>", "n", "N", "*", "#", "?", "/" }
+    local if_search = vim.tbl_contains(keys, vim.fn.keytrans(char))
 
-        vim.opt.hlsearch = if_search
-    end
+    vim.opt.hlsearch = if_search
+  end
 end
 
 vim.on_key(toggle_hlsearch, ns)
--- nvim/lua/plugin/line.lua
+++ nvim/lua/plugin/line.lua
@@ -1,12 +1,12 @@
 return {
-    {
-        "echasnovski/mini.statusline",
-        event = "BufReadPost",
-        config = true,
-    },
-    {
-        "echasnovski/mini.tabline",
-        event = { "VeryLazy" },
-        config = true,
-    },
+  {
+    "echasnovski/mini.statusline",
+    event = "BufReadPost",
+    config = true,
+  },
+  {
+    "echasnovski/mini.tabline",
+    event = { "VeryLazy" },
+    config = true,
+  },
 }
