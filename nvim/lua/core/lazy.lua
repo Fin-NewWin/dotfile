5be870d65f3be5d6f1fcb0f0ec560c8af67c0289
Phien Nguyen <nguyenphien17@yahoo.com>
2023-11-16 19:36:29 -0800

    chore: space to tab

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -1,30 +1,30 @@
 -- Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "git@github.com:folke/lazy.nvim.git",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"git@github.com:folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-    spec = {
-        { import = "plugin" },
-    },
-    defaults = {
-        lazy = true,
-    },
-    checker = {
-        enabled = false,
-    },
-    debug = false,
-    change_detection = {
-        notify = false,
-    },
+	spec = {
+		{ import = "plugin" },
+	},
+	defaults = {
+		lazy = true,
+	},
+	checker = {
+		enabled = false,
+	},
+	debug = false,
+	change_detection = {
+		notify = false,
+	},
 })
