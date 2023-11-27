303b5a2a7c52edf513f6d95c8c6c1522f1a3a642
Phien Nguyen <nguyenphien17@yahoo.com>
2023-11-27 10:51:20 -0800

    chore: update

--- nvim/lua/plugin/util.lua
+++ nvim/lua/plugin/util.lua
@@ -42,6 +42,9 @@ return {
 		"nmac427/guess-indent.nvim",
 		event = "BufReadPost",
 		opts = {},
-		config = true,
+		config = function(_, opts)
+			require("guess-indent").setup(opts)
+			vim.cmd.lua({ args = { "require('guess-indent').set_from_buffer('auto_cmd')" }, mods = { silent = true } })
+		end,
 	},
 }
