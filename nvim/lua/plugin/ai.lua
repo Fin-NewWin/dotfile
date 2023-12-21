ccac0a2f813a6609cf3f7963540cf49681c37d0e
Phien Nguyen <nguyenphien17@yahoo.com>
2023-12-21 03:45:00 -0800

    chore: renable codeium

--- nvim/lua/plugin/ai.lua
+++ nvim/lua/plugin/ai.lua
@@ -1,1 +1,16 @@
-return {}
+return {
+	{
+		"Exafunction/codeium.vim",
+		event = "BufEnter",
+		config = function()
+			vim.g.codeium_disable_bindings = 1
+			vim.keymap.set("i", "<C-Space>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true })
+			-- FIX: xdg is not working
+			-- vim.keymap.set("n", "<C-p>", function()
+			-- 	return vim.fn["codeium#command#HomeDir"]()
+			-- end, { expr = true })
+		end,
+	},
+}
