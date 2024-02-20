39a408cedb73e0de8ace93d1ad857113c3e01f4e
Phien Nguyen <nguyenphien17@yahoo.com>
2024-02-19 18:30:13 -0800

    chore: update

--- nvim/lua/plugin/colorscheme.lua
+++ nvim/lua/plugin/colorscheme.lua
@@ -1,22 +1,24 @@
 return {
 	{
-		"sainnhe/gruvbox-material",
-		name = "gruvbox-material",
+		"ellisonleao/gruvbox.nvim",
 		lazy = false,
-		config = function()
-			vim.g.gruvbox_material_better_performance = 1
-
-			-- Fonts
-			vim.g.gruvbox_material_enable_italic = 1
-			vim.g.gruvbox_material_enable_bold = 1
-			vim.g.gruvbox_material_transparent_background = 1
-			-- Themes
-			vim.g.gruvbox_material_foreground = "mix"
-			vim.g.gruvbox_material_background = "medium"
-			vim.g.gruvbox_material_ui_contrast = "high" -- The contrast of line numbers, indent lines, etc.
-			vim.g.gruvbox_material_float_style = "dim" -- Background of floating windows
-
-			vim.cmd.colorscheme("gruvbox-material")
+		priority = 100,
+		opts = {
+			overrides = {
+				GitSignsCurrentLineBlame = { link = "Comment" },
+				["@lsp.type.function.lua"] = { link = "GruvboxOrange" },
+				NormalFloat = { link = "Pmenu" },
+				Pmenu = { bg = "#3c3836" },
+				MiniCursorword = { bg = "#d7005f", fg = "#ffffff", bold = true, underline = true },
+				MiniCursorwordCurrent = { underline = true },
+				-- "semiSelected" for python
+			},
+			transparent_mode = true,
+			-- contrast = "hard",
+		},
+		config = function(_, opts)
+			require("gruvbox").setup(opts)
+			vim.cmd.colorscheme("gruvbox")
 		end,
 	},
 }
--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -12,14 +12,15 @@ return {
 				ensure_installed = "all",
 				highlight = {
 					additional_vim_regex_highlighting = false,
-					-- disable = function(lang, buf)
-					-- 	local max_filesize = 100 * 1024 -- 100 KB
-					-- 	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-					-- 	return ok and stats and stats.size > max_filesize
-					-- 	-- if ok and stats and stats.size > max_filesize then
-					-- 	-- 	return true
-					-- 	-- end
-					-- end,
+					enable = true,
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						return ok and stats and stats.size > max_filesize
+						-- if ok and stats and stats.size > max_filesize then
+						-- 	return true
+						-- end
+					end,
 				},
 				indent = {
 					enable = true,
