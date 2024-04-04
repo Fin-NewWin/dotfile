9454d15ef85c55fc2986ed32153373bc27fe2650
Phien Nguyen <nguyenphien17@yahoo.com>
2024-04-03 20:51:08 -0700

    chore: change to neocodeium, less lag

--- nvim/lua/plugin/ai.lua
+++ nvim/lua/plugin/ai.lua
@@ -1,19 +1,11 @@
 return {
 	{
-		"Exafunction/codeium.vim",
-		event = "BufEnter",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
+		"monkoose/neocodeium",
+		event = "VeryLazy",
 		config = function()
-			vim.g.codeium_disable_bindings = 1
-			vim.keymap.set("i", "<C-Space>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true })
-			-- FIX: xdg is not working
-			-- vim.keymap.set("n", "<C-p>", function()
-			-- 	return vim.fn["codeium#command#HomeDir"]()
-			-- end, { expr = true })
+			local neocodeium = require("neocodeium")
+			neocodeium.setup()
+			vim.keymap.set("i", "<C-Space>", neocodeium.accept)
 		end,
 	},
 }
