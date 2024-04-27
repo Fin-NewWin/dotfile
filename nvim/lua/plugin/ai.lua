d2ab1fb730f0d91b02f409c7ea9d2c178676b6f2
Phien Nguyen <nguyenphien17@yahoo.com>
2024-04-26 20:17:36 -0700

    feat: codeium disable in rename func

--- nvim/lua/plugin/ai.lua
+++ nvim/lua/plugin/ai.lua
@@ -4,7 +4,15 @@ return {
 		event = "VeryLazy",
 		config = function()
 			local neocodeium = require("neocodeium")
-			neocodeium.setup()
+			neocodeium.setup({
+				enabled = function()
+					local disabled = {
+						IncRename = true,
+					}
+					local cmd = vim.fn.getcmdline():match("%S+")
+					return not disabled[cmd]
+				end,
+			})
 			vim.keymap.set("i", "<C-Space>", neocodeium.accept)
 		end,
 	},
