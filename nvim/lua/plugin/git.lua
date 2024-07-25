b1d041aa75c93074d9774f0ca28233eff1928331
Phien Nguyen <nguyenphien17@yahoo.com>
2024-07-25 10:04:48 -0700

    fix deprecate

--- nvim/lua/plugin/git.lua
+++ nvim/lua/plugin/git.lua
@@ -4,44 +4,44 @@ return {
 	config = function()
 		local sign_text = "▎"
 		require("gitsigns").setup({
-			signs = {
-				add = {
-					hl = "GitSignsAdd",
-					text = sign_text,
-					numhl = "GitSignsAddNr",
-					linehl = "GitSignsAddLn",
-				},
-				change = {
-					hl = "GitSignsChange",
-					text = sign_text,
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				delete = {
-					hl = "GitSignsDelete",
-					text = sign_text,
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				changedelete = {
-					hl = "GitSignsChange",
-					text = sign_text,
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				untracked = {
-					hl = "GitSignsAdd",
-					text = sign_text,
-					numhl = "GitSignsAddNr",
-					linehl = "GitSignsAddLn",
-				},
-				topdelete = {
-					hl = "GitSignsDelete",
-					text = "‾",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-			},
+			-- signs = {
+			-- 	add = {
+			-- 		hl = "GitSignsAdd",
+			-- 		text = sign_text,
+			-- 		numhl = "GitSignsAddNr",
+			-- 		linehl = "GitSignsAddLn",
+			-- 	},
+			-- 	change = {
+			-- 		hl = "GitSignsChange",
+			-- 		text = sign_text,
+			-- 		numhl = "GitSignsChangeNr",
+			-- 		linehl = "GitSignsChangeLn",
+			-- 	},
+			-- 	delete = {
+			-- 		hl = "GitSignsDelete",
+			-- 		text = sign_text,
+			-- 		numhl = "GitSignsDeleteNr",
+			-- 		linehl = "GitSignsDeleteLn",
+			-- 	},
+			-- 	changedelete = {
+			-- 		hl = "GitSignsChange",
+			-- 		text = sign_text,
+			-- 		numhl = "GitSignsChangeNr",
+			-- 		linehl = "GitSignsChangeLn",
+			-- 	},
+			-- 	untracked = {
+			-- 		hl = "GitSignsAdd",
+			-- 		text = sign_text,
+			-- 		numhl = "GitSignsAddNr",
+			-- 		linehl = "GitSignsAddLn",
+			-- 	},
+			-- 	topdelete = {
+			-- 		hl = "GitSignsDelete",
+			-- 		text = "‾",
+			-- 		numhl = "GitSignsDeleteNr",
+			-- 		linehl = "GitSignsDeleteLn",
+			-- 	},
+			-- },
 			sign_priority = 6,
 			current_line_blame = true,
 			current_line_blame_opts = {
